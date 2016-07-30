f4fcc3c19a0c537eebf56031101c10a8b70a1b79
boxiaowave <xbustc@gmail.com>
2016-07-31 00:21:27 +0800

    add a file to test CPU cores

---
+++ Mutiprocess.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#--*-- coding=utf-8 --*--
+
+import os
+import re
+from multiprocessing import Pool
+import time
+import random
+# import pdb
+print 'Prosscess(%s) starts...' % os.getpid()
+'''
+pid = os.fork()
+if pid==0:
+    print 'I am child process (%s) and my parent is %s.'% (os.getpid(),os.getppid())
+else:
+    print 'I (%s) just created a child process(%s).' % (os.getpid(),pid)
+'''
+# k=re.compile(r'(hello)')
+#
+# st=re.search(k,'hello,hghellosds!!')
+#
+# print st.group(1).title()
+
+# from multiprocessing import Process
+#
+# def runc(name):
+#     print 'this is ',name,'-',os.getpid()
+#
+# if __name__=='__main__':
+#     print 'Parent process is ',os.getpid()
+#     p=Process(target=runc,args=('test',))
+#     print 'process starts'
+#     p.start()
+#     p.join()
+#     print 'end child process'
+#     print os.getpid()
+
+
+def long_time(name):
+    print 'run task is',name,'-',os.getpid()
+    start = time.time()
+    time.sleep(random.random()*3)
+    end = time.time()
+    print 'run task (',name,'),for ',end-start
+
+if __name__=='__main__':
+    print 'Parent process is ',os.getpid()
+    p=Pool()
+    for i in range(5):
+        p.apply_async(long_time,args=(i,))
+    print 'wait all subprocesses'
+    p.close()
+    p.join()
+    print 'done'
