41380f00ebbf2a86b5ab3f9be384104b21903fe1
boxiaowave <xbustc@gmail.com>
2016-07-31 12:16:23 +0800

    加入创建子线程实例

--- Mutiprocess.py
+++ Mutiprocess.py
@@ -6,6 +6,7 @@ import re
 from multiprocessing import Pool
 import time
 import random
+import threading
 # import pdb
 print 'Prosscess(%s) starts...' % os.getpid()
 '''
@@ -36,19 +37,38 @@ else:
 #     print os.getpid()
 
 
-def long_time(name):
-    print 'run task is',name,'-',os.getpid()
-    start = time.time()
-    time.sleep(random.random()*3)
-    end = time.time()
-    print 'run task (',name,'),for ',end-start
+# def long_time(name):
+#     print 'run task is',name,'-',os.getpid()
+#     start = time.time()
+#     time.sleep(random.random()*3)
+#     end = time.time()
+#     print 'run task (',name,'),for ',end-start
+#
+# if __name__=='__main__':
+#     print 'Parent process is ',os.getpid()
+#     p=Pool()
+#     for i in range(5):
+#         p.apply_async(long_time,args=(i,))
+#     print 'wait all subprocesses'
+#     p.close()
+#     p.join()
+#     print 'done'
+
+def loop(s):
+    print 'thread '+threading.current_thread().name+' starts running'
+    print s
+    for n in range(5):
+        print 'thread'+threading.current_thread().name,'>>',n
+        time.sleep(1)
+    print 'thread '+threading.current_thread().name+' ends'
 
 if __name__=='__main__':
-    print 'Parent process is ',os.getpid()
-    p=Pool()
-    for i in range(5):
-        p.apply_async(long_time,args=(i,))
-    print 'wait all subprocesses'
-    p.close()
-    p.join()
-    print 'done'
+    print 'thread ' + threading.current_thread().name + ' starts running'
+    t=threading.Thread(target=loop,name='child',args=('test',))
+    t.start()
+    t.join()
+    print 'thread ' + threading.current_thread().name + ' ends'
+
+
+
+
