513822d2579e2a714304875097f18c1cc69f018b
boxiaowave <xbustc@gmail.com>
2016-08-12 19:22:02 +0800

    None.

--- InsertSort.py
+++ InsertSort.py
@@ -7,14 +7,15 @@ Created on Fri Aug 12 17:14:42 2016
 
 def InsertSort(li):
     for i in range(1,len(li)):
-        temp = li[i]
-        print li[i]
-        for j in range(i):
-            if temp<li[i-j-1]:
-                li[i-j]=li[i-j-1]
-                li[i-j-1] = temp
-            else:
-                break
+        if li[i]>li[i-1]:
+            temp = li[i]
+            print li[i]
+            for j in range(i):
+                if temp<li[i-j-1]:
+                    li[i-j]=li[i-j-1]
+                    li[i-j-1] = temp
+                else:
+                    break
     return li
     
 li = [12,11,13,9,7,4,2,1]
